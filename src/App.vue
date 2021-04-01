50666afa93b51709919af1cf3ad8bfa8a8840474
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-01 20:31:04 -0300

    feat: add background and foreground division to app component layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="background" />
+  <div class="foreground">
+    <Header />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+
+  * {
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+
+  #app {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .background {
+    background-color: #266E3F;
+    border-top-left-radius: 100%;
+    bottom: 0;
+    height: 75.9vh;
+    position: absolute;
+    right: 0;
+    width: 38.66vw;
+    z-index: -1;
+  }
+
+  .foreground {
+    z-index: 1;
+  }
 </style>
