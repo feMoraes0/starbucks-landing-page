8a474d871f10b1fcf6d91b20c075f9a8064b50e4
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-01 19:28:56 -0300

    feat: add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <header>
+    <img src="../assets/logo.png" alt="Starbucks Logo">
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Menu</a></li>
+      <li>
+        <a href="#">What's New</a>
+      </li>
+      <li>
+        <a href="#">Contact Us</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="css" scoped>
+  header {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-left: 99px;
+    padding-right: 84px;
+    padding-top: 18px;
+    width: 100%;
+  }
+
+  img {
+    width: 82px;
+    height: 82px;
+  }
+
+  li a {
+    color: #453D39;
+  }
+
+  li {
+    border: 1px solid white;
+    display: inline;
+    font-weight: 400;
+    margin-right: 40px;
+  }
+
+  li:last-child {
+    margin-right: 0;
+  }
+</style>
