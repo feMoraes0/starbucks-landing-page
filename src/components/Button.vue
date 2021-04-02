ca244b614c268bc7d0132506ba34da0c557c270a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-01 21:41:54 -0300

    feat: add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <button type="button" name="button">
+    {{content}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    content: String
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  button {
+    background-color: #266E3F;
+    border: none;
+    border-radius: 36px;
+    color: #FFFFFF;
+    font-size: 1.2rem;
+    line-height: 1.2rem;
+    padding: 12px 24px;
+  }
+</style>
