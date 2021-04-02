7a9792e7215b9ccccae12243ddc2f8f0ea144dd0
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-01 21:42:38 -0300

    feat: add previous created components to main component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,29 @@
   <div class="background" />
   <div class="foreground">
     <Header />
+    <Content />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Content from "./components/Content.vue";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+
 
 export default {
   name: 'App',
   components: {
+    Content,
+    Footer,
     Header,
   }
 }
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
 
   * {
     box-sizing: border-box;
@@ -44,6 +51,11 @@ export default {
   }
 
   .foreground {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: space-between;
+    width: 100vw;
     z-index: 1;
   }
 </style>
