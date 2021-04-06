67e58f6fae05c67872a6f0c942ef634242251858
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-05 22:34:02 -0300

    test: add button component unit test

---
+++ tests/unit/Button.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from "@vue/test-utils";
+import Button from "@/src/components/Button.vue";
+
+describe("Button component", () => {
+  const wrapper = shallowMount(Button, {
+    propsData:{
+      content: "any_content",
+      backgroundColour: "any_background_colour"
+    }
+  });
+
+  it("should initialize button component correctly", () => {
+    // THEN
+    expect(wrapper.find("button").exists()).toBe(true);
+  });
+
+  it("should validate received properties", () => {
+    // THEN
+    expect(wrapper.props().content).toEqual("any_content");
+    expect(wrapper.props().backgroundColour).toEqual("any_background_colour");
+  });
+
+  it("should validate watch function", async () => {
+    // GIVEN
+    const spy = jest.spyOn(document, "querySelector").mockReturnValueOnce({ style: { backgroundColor: "" }});
+    await wrapper.setProps({backgroundColour: "another_background_colour"});
+    // THEN
+    expect(wrapper.props().backgroundColour).toEqual("another_background_colour");
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
