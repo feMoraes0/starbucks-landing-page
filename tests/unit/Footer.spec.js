c1739de611774be6addbf45a7f59814fb13e0b4a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-06 18:30:41 -0300

    test: add Footer unit test

---
+++ tests/unit/Footer.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import Footer from "@/src/components/Footer.vue";
+
+describe("Footer component", () => {
+  const wrapper = shallowMount(Footer);
+
+  it("should trigger a parent function via emit function", async () => {
+    // WHEN
+    wrapper.vm.updateProduct("any_product");
+    // THEN
+    expect(wrapper.emitted().updateProductNumber).toBeTruthy();
+    expect(wrapper.emitted().updateProductNumber.length).toBe(1);
+    expect(wrapper.emitted().updateProductNumber[0]).toEqual(["any_product"]);
+  });
+});
